530c0722a0def12bb3d8cc82812862676a384ddd
jigyasakaur22212 <67827565+jigyasakaur22212@users.noreply.github.com>
2024-03-29 10:33:50 +0000

    chapters done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,2 +1,28 @@
 "use strict" // treat all js code AS NEWER VERSIONS
 
+//alert(3+3) // this is not used as earlier js was used in embedded form and inside the search engine
+
+// console.log(3+3); console.log("jigyasa")// this way of writing reduces redeability 
+
+//this is better way and clean way
+console.log(3+3)
+
+console.log("jigyasa")
+
+let name = "Jigyasa"
+let age = 18
+let isLonggedIn = false
+let state = null
+
+// number => 2 ^ 53
+// bigint
+//string =>""
+// boolean => True and False
+// null => standalone value (empty)
+//undefined => value is not assigned 
+//symbol => find the uniqueness 
+//object =>
+
+console.log(typeof age);
+console.log(typeof undefined)//undefined 
+console.log(typeof null)//object

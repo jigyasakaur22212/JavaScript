96c67f0df822a9e5464ef546a4ce4cff04e1935f
jigyasakaur22212 <67827565+jigyasakaur22212@users.noreply.github.com>
2024-03-30 23:27:08 +0000

    chapter done

--- basics/operations.js
+++ basics/operations.js
@@ -1,46 +1,85 @@
-let age = "10abc"
-let score = "33"
-let temp = null
-let test;
+// let age = "10abc"
+// let score = "33"
+// let temp = null
+// let test;
 
-console.log(typeof score);
-console.log(typeof(age));
+// console.log(typeof score);
+// console.log(typeof(age));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-valueInNumber = Number(age)
-console.log(typeof valueInNumber);
-console.log(valueInNumber) // here it is returning the NAN value when 
+// valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) // here it is returning the NAN value when 
 
-valueInNumber = Number(temp)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// valueInNumber = Number(temp)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-valueInNumber = Number(test)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// valueInNumber = Number(test)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// here conversions are taking place as default and leading to errors in the value during the output 
+// // here conversions are taking place as default and leading to errors in the value during the output 
 
-/*
-"33"=> 33
-"12asd" => Nan
-true = 1,false = 0
-*/
+// /*
+// "33"=> 33
+// "12asd" => Nan
+// true = 1,false = 0
+// */
 
-let isloggedIn = 1 // on converting get true
+// let isloggedIn = 1 // on converting get true
 
-let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
 
-isloggedIn = "HIte"
+// isloggedIn = "HIte"
 
-booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn); // here we are not giving 1/0 then to o/p is true 
+// booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn); // here we are not giving 1/0 then to o/p is true 
 
-let somenum = 33
+// let somenum = 33
 
-let stringnum = String(somenum)
-console.log(stringnum);
+// let stringnum = String(somenum)
+// console.log(stringnum);
+
+
+//***************operations************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2)
+console.log(2/2);
+console.log(2%2);//use in cryptography and algorithms 
+
+let str1 = "Hello "
+let str2 = "Jigyasa"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+"2"+2);
+console.log(1+2+"2");//ToPrimitive - acme rule 
+console.log(3+4*5/6);
+
+console.log(true)
+console.log(+true);//this conversions are tricky and not prefered 
+console.log(+"");
+
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+

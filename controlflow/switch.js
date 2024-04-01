58847abcb0e63d0588a83108ac49761ff9400199
jigyasakaur22212 <67827565+jigyasakaur22212@users.noreply.github.com>
2024-04-01 15:34:28 +0000

    chapter done

---
+++ controlflow/switch.js
@@ -0,0 +1,56 @@
+// switch (key -> for this we check multiple values) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// break breaks the control flow - if not then it will execute all the switch cases except Default
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("MAy");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        break;
+}
+
+//similarly we can do with string but remember to do with case sensitivity 
+//here we can avoid operators
